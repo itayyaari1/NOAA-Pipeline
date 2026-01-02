894edd6409de4895ebdbe090717c3764873abde1
itayyaari <itayyaari1@gmail.com>
2026-01-02 14:57:47 +0200

    Add schema and table creation functions for the data pipeline
    - Implemented functions to create database schemas and tables for raw data, metrics, and watermarks in ddl.py.
    - Each function ensures the creation of tables with specified columns and data types, using the PARQUET format for storage.

---
+++ src/db/ddl.py
@@ -0,0 +1,41 @@
+def create_schema(schema: str) -> str:
+    return f"""CREATE SCHEMA IF NOT EXISTS {schema}"""
+
+def create_raw_table(schema: str, table: str) -> str:
+    return f"""
+    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+        station_id VARCHAR,
+        datatype VARCHAR,
+        value BIGINT,
+        event_time_ms BIGINT,
+        attributes ARRAY(VARCHAR),
+        source_record JSON,
+        ingestion_ts BIGINT
+    )
+    WITH (
+        format = 'PARQUET'
+    )
+    """
+
+def create_metrics_table(schema: str, table: str) -> str:
+    return f"""
+    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+        station_id VARCHAR,
+        total_observations BIGINT,
+        missing_observations BIGINT,
+        missing_percentage DOUBLE,
+        last_updated_ts BIGINT
+    )
+    WITH (format='PARQUET')
+    """
+
+def create_watermark_table(schema: str, table: str) -> str:
+    return f"""
+    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+        job_name VARCHAR,
+        last_processed_ingestion_ts BIGINT,
+        updated_ts BIGINT
+    )
+    WITH (format='PARQUET')
+    """
+
